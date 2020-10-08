7b9809b6dae9bd7831876b0324d4999b5eceb956
victor <joao.chagas@viasoft.com.br>
2020-10-07 22:04:53 -0300

    Fixed all the unit tests for newsletter subscription

--- src/test/java/io/github/gasparbarancelli/blog/subscriptionTest/SubscriptionControllerTest.java
+++ src/test/java/io/github/gasparbarancelli/blog/subscriptionTest/SubscriptionControllerTest.java
@@ -1,9 +1,6 @@
 package io.github.gasparbarancelli.blog.subscriptionTest;
 
-import io.github.gasparbarancelli.blog.newsletter.NewsLetterData;
-import io.github.gasparbarancelli.blog.newsletter.NewsLetterRegistrationController;
-import io.github.gasparbarancelli.blog.newsletter.NewsLetterService;
-import io.github.gasparbarancelli.blog.newsletter.NewsLetterSubscription;
+import io.github.gasparbarancelli.blog.newsletter.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,13 +8,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class SubscriptionControllerTest {
 
     @InjectMocks
     private NewsLetterRegistrationController newsLetterRegistrationController;
+
+    @InjectMocks
+    private NewLetterServiceImpl newsLetterServiceImpl;
 
     @Mock
     private NewsLetterService newsLetterService;
@@ -29,23 +32,33 @@ public class SubscriptionControllerTest {
 
     @Test
     @DisplayName("make sure the subscribe new user in service is called")
-    private void makeSureServiceIsCalled() {
+    public void makeSureServiceIsCalled() {
         newsLetterRegistrationController.subscribeInNewsLetter(email);
-
-        newsLetterService.subscribeNewUser(email);
+        verify(newsLetterService).subscribeNewUser(email);
     }
 
     @Test
     @DisplayName("make sure the save is called in data layer")
-    private void makeSureSaveIsCalled() {
-        newsLetterService.subscribeNewUser(email);
-
-        newsLetterData.save(new NewsLetterSubscription(email));
+    public void makeSureSaveIsCalled() {
+        newsLetterServiceImpl.subscribeNewUser(email);
+        verify(newsLetterData).save(any(NewsLetterSubscription.class));
     }
 
     @Test
     @DisplayName("throw exception for invalid mails")
-    private void thorwsIllegalArgumentExceptionIfEmailIsNotValid() {
-        assertThrows(IllegalArgumentException.class, () -> newsLetterService.subscribeNewUser("john"));
+    public void thorwsIllegalArgumentExceptionIfEmailIsNotValid() {
+        assertThrows(IllegalArgumentException.class, () -> newsLetterServiceImpl.subscribeNewUser("john"));
+    }
+
+    @Test
+    @DisplayName("throw exception for invalid mails")
+    public void thorwsIllegalArgumentExceptionIfEmailIsEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> newsLetterServiceImpl.subscribeNewUser(""));
+    }
+
+    @Test
+    @DisplayName("does not throw any execpton for valid emails")
+    public void doesNotThrowsForValidEmails() {
+        assertDoesNotThrow(() -> newsLetterServiceImpl.subscribeNewUser("jvrc2000@outlook.com"));
     }
 }
